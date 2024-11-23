8e53fcd85d625c72ac1104a4d39bb30547d3878b
hashjenny <huangxudong717@outlook.com>
2024-11-23 17:23:53 +0800

    lab01: finished

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,26 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (!head) {
+        return 0;
+    }
+    node *tortoise = head;
+    node *hare = head;
+    while (tortoise -> next) {
+        for (int i = 0; i < 2; ++i) {
+            if (hare -> next) {
+                hare = hare -> next;
+                if (hare == tortoise) {
+                    return 1;
+                }
+            } else {
+                return 0;
+            }
+        }
+        tortoise = tortoise -> next;
+        if (hare == tortoise) {
+            return 1;
+        }
+    }
     return 0;
 }
--- lab01/segfault_ex.c
+++ lab01/segfault_ex.c
@@ -1,6 +1,7 @@
 int main() {
     int a[20];
-    for (int i = 0; ; i++) {
+//    for (int i = 0; ; i++) {
+    for (int i = 0; i < sizeof(a) / sizeof(a[0]); i++) {
         a[i] = i;
     }
 }
